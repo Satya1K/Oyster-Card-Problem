338b2f79f8dffa6aad5ffeb1f36f9eedd019e04d
Satya Suman <42642478+Satya1K@users.noreply.github.com>
2022-04-23 03:35:53 +0530

    Add files via upload

---
+++ src/test/java/com/satya/OysterCardApp.java
@@ -0,0 +1,38 @@
+package com.satya.app;
+
+import unitEntity.Card;
+import unitEntity.Journey;
+import oysterEnums.Transport;
+import oysterEnums.StationZone;
+
+/**
+ *Satya-Oyster Card Problem
+ *
+ */
+public class OysterCardApp 
+{
+    public static void main( String[] args )
+    {
+       Card card = new Card();
+       System.out.println("Loading a card with £30");
+       card.addMoney(30);
+       
+       Journey jorneyHolbornToCourt = new Journey();
+       jorneyHolbornToCourt.setStartPoint(Transport.TUBE,StationZone.HOLBORN,card);
+       jorneyHolbornToCourt.setEndPoint(StationZone.EARLS_COURT);
+              
+       System.out.printf("Card Balance after first journey (Tube Holborn to Earl’s Court)  is : %s \n",card.getBalance());
+       
+       Journey jorneyBusEarlToChelsea = new Journey();
+       jorneyBusEarlToChelsea.setStartPoint(Transport.BUS,null,card);
+       jorneyBusEarlToChelsea.setEndPoint(null);
+       
+       System.out.printf("Card Balance after second journey (328 bus from Earl’s Court to Chelsea) is : %s \n",card.getBalance());
+       
+       Journey jorneyEarlsToHammersmith = new Journey();
+       jorneyEarlsToHammersmith.setStartPoint(Transport.TUBE, StationZone.EARLS_COURT, card);
+       jorneyEarlsToHammersmith.setEndPoint(StationZone.HAMMERSMITH);
+       
+       System.out.printf("Card Balance after third journey (Tube Earl’s court to Hammersmith)  is : %s \n",card.getBalance());
+    }
+}
